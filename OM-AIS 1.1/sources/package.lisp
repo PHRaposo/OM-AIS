d0ba1f89795292b550e6d0fd9bf3b4b15bb882f2
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2021-05-01 18:39:09 -0300

    Add files via upload

---
+++ OM-AIS 1.1/sources/package.lisp
@@ -0,0 +1,14 @@
+(in-package :om)
+
+(defvar *OM-AIS-path* (make-pathname  :directory (append (pathname-directory *load-pathname*) (list "AIS"))))
+
+(require-library "Mathtools")
+
+(defpackage :OM-AIS
+(:use "COMMON-LISP" "OM" "CL-USER"))
+
+
+
+
+
+
