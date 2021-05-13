20fc9ddf4373d12e6a228cd06b5e67ddc685ba37
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2021-05-13 12:09:08 -0300

    Update package.lisp

--- OM-AIS 1.1/sources/package.lisp
+++ OM-AIS 1.1/sources/package.lisp
@@ -2,7 +2,7 @@
 
 (defvar *OM-AIS-path* (make-pathname  :directory (append (pathname-directory *load-pathname*) (list "AIS"))))
 
-(require-library "Mathtools")
+;(require-library "Mathtools")
 
 (defpackage :OM-AIS
 (:use "COMMON-LISP" "OM" "CL-USER"))
